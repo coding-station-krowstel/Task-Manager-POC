3575b9acafc895588c283572e0143f0b67bc968a
Coding Station <saurabhsingh.sitcomp@gmail.com>
2026-07-25 13:33:43 +0530

    add server rendered add task page

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('tasks/', views.TaskListCreateView.as_view(), name="task-list"),
     path('tasks/<int:pk>/',views.TaskDetailView.as_view(),name="task_detail_api"),
     path('',views.task_list,name='task_list'),
-    path("task/<int:pk>/",views.task_detail,name="task_detail")
+    path("task/<int:pk>/",views.task_detail,name="task_detail"),
+    path("task/add/",views.add_task,name="add_task")
 ]
--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
+from .forms import TaskForm
 
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
@@ -18,6 +19,16 @@ def task_list(request):
 
 def task_detail(request,pk):
     task = get_object_or_404(Task,pk=pk)
-    return render(request,"tasks/task_detail.html",{"task":task})   
+    return render(request,"tasks/task_detail.html",{"task":task})  
+
+def add_task(request):
+    if request.method=="POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form=TaskForm()
+    return render(request,"tasks/add_task.html",{"form":form})         
 
 
